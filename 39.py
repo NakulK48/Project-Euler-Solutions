0ed5856d8b4f60208548c02bc5c89a4e2d0838d0
Nakul Khanna <NakulK48@users.noreply.github.com>
2017-02-25 20:44:55 +0000

    Update 39.py

--- 39.py
+++ 39.py
@@ -1,23 +1,28 @@
-#PROBLEM 39
+# PROBLEM 39
 
-triples = [[ 3 , 4 , 5 ], [ 5, 12, 13], [ 7, 24, 25], [ 8, 15, 17], [ 9, 40, 41], [11, 60, 61], [12, 35, 37], [13, 84, 85], [16, 63, 65], [20, 21, 29], [28, 45, 53], [33, 56, 65], [36, 77, 85], [39, 80, 89], [48, 55, 73], [65, 72, 97]]
+import math
 
-triplemins = []
+squares = set()
+  
+MAX_SIDE = 1000
 
-print(triples[0])
+perimeterCounts = {}
 
-trip = []
+for i in xrange(1000):
+  squares.add(i**2)
 
-for i in range(66):
-    trip.append(0)
-
-for sublist in triples:
-    a = sublist[0]
-    b = sublist[1]
-    c = sublist[2]
-    d = [b,c]
-    triplemins.append(a)
-    trip[a] = d
-
-print(trip)
-    
+for a in xrange(MAX_SIDE):
+  for b in xrange(MAX_SIDE):
+    cSquared = a**2 + b**2
+    if cSquared in squares:
+      p = a+b+int(math.sqrt(cSquared))
+      if (p > 1000):
+        continue
+      if p not in perimeterCounts:
+        perimeterCounts[p] = 0
+      perimeterCounts[p] += 1
+      
+mostCommonPerimeter = max(perimeterCounts, 
+  key=perimeterCounts.get)
+  
+print mostCommonPerimeter
